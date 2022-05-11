20c008beadc25f66ec8ba3dc1370ba0a0d5bd779
John <professional-contact@johnmanuel.info>
2022-05-11 13:41:24 -0700

    Minor changes
    changing the text for the about page and also testing out various methods for implementing functions in JS

---
+++ MultiPage Website/JS/formSubmit.js
@@ -0,0 +1,7 @@
+function submitForm(fname, lname, occupation, country){
+    window.alert("you submitted: \n"
+                 + fname + "\n"
+                 + lname + "\n"
+                 + occupation + "\n"
+                 + country);
+}
